0b65889b20285a3cf22cedbb6e89a7a151df7cf6
Kai <S364492@yo-woo.com>
2023-12-02 01:44:34 +0800

    day01: upload part1 solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,26 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var sum = 0
+        input.forEach{
+            var firstDigit = -1
+            for(i in it.indices) {
+                val curr = it[i] -'0'
+                if (curr in 0..9){
+                    firstDigit = curr
+                    break
+                }
+            }
+            var lastDigit = -1
+            for(i in it.length-1 downTo 0) {
+                val curr = it[i] -'0'
+                if (curr in 0..9){
+                    lastDigit = curr
+                    break
+                }
+            }
+            sum += firstDigit*10 + lastDigit
+        }
+        return sum
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +29,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 142)
 
     val input = readInput("Day01")
     part1(input).println()
