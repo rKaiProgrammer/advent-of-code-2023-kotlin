bb3a6b970447d1d386e8709d14c121a42fc7ddb3
Kai <S364492@yo-woo.com>
2023-12-03 01:36:30 +0800

    day02: upload part2 solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -35,13 +35,33 @@ fun main() {
 
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val colors = listOf( "red", "green", "blue")
+        var sum = 0
+        input.forEach{
+            val substring = it.substring(it.indexOf(':') + 2, it.length)
+            val maxNumArr = IntArray(3)
+            substring.split("; ").forEach{ round->
+                round.split(", ").forEach {
+                    val num = it.substring(0, it.indexOf(" ")).toInt()
+                    for(i in colors.indices) {
+                        val color = colors[i]
+                        if (it.contains(color)){
+                            maxNumArr[i] = Math.max(maxNumArr[i], num)
+                            break
+                        }
+                    }
+                }
+            }
+            val power = maxNumArr[0] * maxNumArr[1] * maxNumArr[2]
+            sum += power
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 8)
-
+    check(part2(testInput) == 2286)
     val input = readInput("Day02")
     part1(input).println()
     part2(input).println()
